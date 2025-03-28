990925722d407c51588458a2833751c576a73323
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2025-03-28 15:18:02 -0300

    adasd dddd

--- atv1/matriz.c
+++ atv1/matriz.c
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-void main(){
+void calcular_matriz(int n){
     // Input do tamanho da matriz
-    int n = 1200;
+    //int n = 3500;
     
     // Construção da matriz 
-    int matriz[n][n];
-    int vetor[n];
+    int *matriz = (int *)malloc(n*n * sizeof(int *));
+    // Dynamic allocation for vectors
+    int *vetor = (int *)malloc(n * sizeof(int));
+    int *nova_matriz = (int *)malloc(n * sizeof(int));
     int c=1;
     for(int i=0;i<n;i++){
         vetor[i] = i%10;
         for(int j=0;j<n;j++){
-            matriz[i][j] = i-j;
+            matriz[i*n+j] = i-j;
             c++;
             //printf("%d ", matriz[i][j]);
         }
         //printf("\n");
     }
     //printf("===================================================================");
-
-    int nova_matriz[n];
     int soma = 0;
     struct timespec begin, end;
     clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
     for(int i=0;i<n;i++){
         soma = 0;
         for(int j=0;j<n;j++){
-            soma += matriz[i][j] * vetor[j];
+            soma += matriz[i*n+j] * vetor[j];
             //printf("i = %d, j = %d\n", i, j);
             //printf("%d * %d = %d\n", matriz[i][j], vetor[j], matriz[i][j] * vetor[j]);
         }
@@ -36,19 +36,19 @@ void main(){
         soma=0;
     }
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    //for(int i=0;i<n;i++){
-    //    printf("%d ", vetor[i]);
-    //}
-    
-    //printf("===================================================================");
-    
-    //for(int i=0;i<n;i++){
-    //    printf("%d ", nova_matriz[i]);
-    //}
     printf("===================================================================");
     printf("\nMultiplicação de matriz %d por %d por vetor de tamanho %d\n", n, n, n);
     printf ("Total time = %f seconds\n",
             (end.tv_nsec - begin.tv_nsec) / 1000000000.0 +
             (end.tv_sec  - begin.tv_sec));
     printf("\n");
+}
+int main(){
+    int sizes[] = {100, 200, 350, 1000, 3000,4000, 5000,6000,7000};
+    int n;
+    for (int i = 0; i < sizeof(sizes)/sizeof(sizes[0]); i++) {
+        n = sizes[i];
+        calcular_matriz(n);
+        printf("===================================================================\n");
+    }
 }
