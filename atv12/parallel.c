963c96eba7eeab9cef467745e57db9bdca8befe9
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2025-05-27 20:09:07 -0300

    fix atv12

--- atv12/parallel.c
+++ atv12/parallel.c
@@ -50,12 +50,14 @@ float calculate_laplacian_periodic(float *scalar_field_data, int x_coord, int y_
 
 // Function to advance the simulation by one time step for a given scalar field
 void perform_time_step(float *current_field_data, float *next_field_data, int num_threads, int load_multiplier) {
+    int actual_grid_size_x = load_multiplier * GRID_SIZE_X;
+
     #pragma omp parallel for num_threads(num_threads) collapse(3) schedule(guided,32)
     for (int ix = 0; ix < load_multiplier*GRID_SIZE_X; ix++) {
         for (int iy = 0; iy < GRID_SIZE_Y; iy++) {
             for (int iz = 0; iz < GRID_SIZE_Z; iz++) {
                 int flat_index = MAP_3D_TO_1D_INDEX(ix, iy, iz);
-                float laplacian_at_point = calculate_laplacian_periodic(current_field_data, ix, iy, iz);
+                float laplacian_at_point = calculate_laplacian_periodic(current_field_data, ix, iy, iz, actual_grid_size_x);
                 next_field_data[flat_index] = current_field_data[flat_index] +
                                             DIFFUSION_COEFFICIENT * TIME_STEP_INCREMENT * laplacian_at_point;
             }
