6f86212b45a816b3394707894738195c50dfb6db
root <root@code.localdomain>
2025-05-31 14:02:02 -0300

    update atv14

--- atv14/message.c
+++ atv14/message.c
@@ -1,70 +1,85 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 
-#define NUM_EXCHANGES 5
-#define MESSAGE_CONTENT 42
+#define MESSAGE_SIZE 1024 * 1024// Exemplo: 1 KB = 1024
 
-int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);
 
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+int main(int argc, char *argv[]) {
+    int rank, size;
 
-    printf("Numero do processo: %d\n", world_rank);
-    printf("Tamanho da lista: %d\n", world_size);
-
-    if (world_size < 2) {
-        fprintf(stderr, "Este programa deve ser executado com pelo menos 2 processos.\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
-
-    int message = 55;
+    char message_sent[MESSAGE_SIZE];
+    char message_received[MESSAGE_SIZE];
+    MPI_Status status;
     double start_time, end_time, total_time;
 
-    // Sincroniza os processos antes de iniciar o temporizador
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    // Inicia o temporizador no processo 0
-    if (world_rank == 0) {
-        start_time = MPI_Wtime();
-    }
-
-    for (int i = 0; i < NUM_EXCHANGES; ++i) {
-        if (world_rank == 0) {
-            // O processo 0 envia a mensagem para o processo 1
-            printf("Mensagem de tamanho %ld enviado pelo processo %d para o processo %d",sizeof(message),world_rank,1);
-            MPI_Send(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-
-            // O processo 0 recebe a mensagem de volta do processo 1
-            MPI_Recv(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Mensagem de tamanho %ld recebido pelo processo %d do processo %d",sizeof(message),world_rank,1);
-
-        } else if (world_rank == 1) {
-            // O processo 1 recebe a mensagem do processo 0
-            MPI_Recv(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            // O processo 1 envia a mesma mensagem de volta para o processo 0
-            MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    // Define numero de trocas como 1000 se não for declarado
+    int num_exchanges = 1000;
+    if (argc > 1) {
+        num_exchanges = atoi(argv[1]);
+        if (num_exchanges <= 0) {
+            num_exchanges = 1000;
         }
     }
 
-    // Sincroniza os processos antes de parar o temporizador
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Para o temporizador e calcula o tempo total no processo 0
-    if (world_rank == 0) {
-        end_time = MPI_Wtime();
-        total_time = end_time - start_time;
-        printf("Tempo total para %d trocas de mensagens: %f segundos\n", NUM_EXCHANGES, total_time);
-        printf("Tempo médio por troca: %f microssegundos\n", (total_time / NUM_EXCHANGES) * 1e6);
+    if (size != 2) {
+        if (rank == 0) {
+            fprintf(stderr, "Erro: Este programa deve ser executado com exatamente 2 processos.\n");
+            fprintf(stderr, "Uso: mpirun -np 2 %s [numero_de_trocas]\n", argv[0]);
+        }
+        MPI_Finalize();
+        return 1;
     }
 
-    // Finaliza o ambiente MPI
-    MPI_Finalize();
+    // Fill mensagem com letras
+    for (int i = 0; i < MESSAGE_SIZE; i++) {
+        message_sent[i] = 'A' + (i % 26); // Preenche com A-Z repetidamente
+    }
 
+    int message_length = MESSAGE_SIZE;
+    MPI_Barrier(MPI_COMM_WORLD); // Sincroniza os processos antes de iniciar o timer
+
+    if (rank == 0) {
+        printf("Iniciando %d trocas de mensagens (ping-pong).\n", num_exchanges);
+        printf("Tamanho da mensagem: %d bytes\n", message_length);
+        start_time = MPI_Wtime();
+
+        for (int i = 0; i < num_exchanges; i++) {
+            // 1. Processo 0 envia para o processo 1
+            MPI_Send(message_sent, message_length, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+
+            // 4. Processo 0 recebe do processo 1
+            MPI_Recv(message_received, message_length, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
+        }
+
+        end_time = MPI_Wtime();
+        total_time = end_time - start_time;
+
+        printf("--------------------------------------------------\n");
+        printf("Tempo total para %d trocas: %f segundos\n", num_exchanges, total_time);
+        printf("Tempo médio por troca (RTT): %e segundos\n", total_time / num_exchanges);
+        if (message_length > 0 && total_time > 0) {
+            double total_data_moved_one_way_gb = (double)num_exchanges * message_length / (1024.0*1024.0*1024.0);
+            double effective_bandwidth_gbps = (total_data_moved_one_way_gb * 2) / total_time; // GB/s, *2 para ping-pong
+            printf("Largura de banda efetiva (ping-pong): %.3f GB/s\n", effective_bandwidth_gbps * 8); // Gbps
+        }
+        printf("--------------------------------------------------\n");
+
+    } else if (rank == 1) {
+        for (int i = 0; i < num_exchanges; i++) {
+            // 2. Processo 1 recebe do processo 0
+            MPI_Recv(message_received, message_length, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+
+            // 3. Processo 1 envia a mesma mensagem de volta para o processo 0
+            MPI_Send(message_received, message_length, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    MPI_Finalize();
     return 0;
 }
-//mpicc message.c -o message
-//mpirun ./message --allow-run-as-root
