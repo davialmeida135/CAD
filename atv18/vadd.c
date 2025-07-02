a0d5b446780d1783d273e2aca4bfa35d50bedf8d
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2025-07-01 21:04:59 -0300

    stack arrays

--- atv18/vadd.c
+++ atv18/vadd.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
-#define N 10000000
+#define N 100000
 #define TOL  0.0000001
 //
 //  This is a simple program to add two vectors
@@ -11,64 +10,50 @@
 //
 int main()
 {
-    printf("Number of OpenMP devices: %d\n", omp_get_num_devices());
-
-    // Allocate arrays dynamically on the heap instead of stack
-    float *a = (float*)malloc(N * sizeof(float));
-    float *b = (float*)malloc(N * sizeof(float));
-    float *c = (float*)malloc(N * sizeof(float));
-    float *res = (float*)malloc(N * sizeof(float));
-    
-    if (!a || !b || !c || !res) {
-        printf("Memory allocation failed!\n");
-        return 1;
-    }
-
+    printf("Number of devices: %d\n", omp_get_num_devices());
+    float a[N], b[N], c[N], res[N];
     int err=0;
 
     double init_time, compute_time, test_time;
-    init_time = -omp_get_wtime();
+    init_time    = -omp_get_wtime();
 
-    // fill the arrays
-    for (int i=0; i<N; i++){
-        a[i] = (float)i;
-        b[i] = 2.0*(float)i;
-        c[i] = 0.0;
-        res[i] = i + 2*i;
-    }
+   // fill the arrays
+   #pragma omp parallel for
+   for (int i=0; i<N; i++){
+      a[i] = (float)i;
+      b[i] = 2.0*(float)i;
+      c[i] = 0.0;
+      res[i] = i + 2*i;
+   }
 
-    init_time += omp_get_wtime();
-    compute_time = -omp_get_wtime();
-    
-    // add two vectors using GPU offloading
-    #pragma omp target parallel for
-    for (int i=0; i<N; i++){
-        c[i] = a[i] + b[i];
-    }
+   init_time    +=  omp_get_wtime();
+   compute_time  = -omp_get_wtime();
+   
+   // add two vectors
+   #pragma omp target
+   #pragma omp loop
+   for (int i=0; i<N; i++){
+      c[i] = a[i] + b[i];
+   }
 
-    compute_time += omp_get_wtime();
-    test_time = -omp_get_wtime();
+   compute_time +=  omp_get_wtime();
+   test_time     = -omp_get_wtime();
 
-    // test results
-    for(int i=0; i<N; i++){
-        float val = c[i] - res[i];
-        val = val*val;
-        if(val>TOL) err++;
-    }
+   // test results
+   #pragma omp parallel for reduction(+:err)
+   for(int i=0;i<N;i++){
+      float val = c[i] - res[i];
+      val = val*val;
+      if(val>TOL) err++;
+   }
 
-    test_time += omp_get_wtime();
-    
-    printf("Vectors added with %d errors\n", err);
-    printf("Init time:    %.3fs\n", init_time);
-    printf("Compute time: %.3fs\n", compute_time);
-    printf("Test time:    %.3fs\n", test_time);
-    printf("Total time:   %.3fs\n", init_time + compute_time + test_time);
-    
-    // Free allocated memory
-    free(a);
-    free(b);
-    free(c);
-    free(res);
-    
-    return 0;
+   test_time    +=  omp_get_wtime();
+   
+   printf(" vectors added with %d errors\n",err);
+
+   printf("Init time:    %.3fs\n", init_time);
+   printf("Compute time: %.3fs\n", compute_time);
+   printf("Test time:    %.3fs\n", test_time);
+   printf("Total time:   %.3fs\n", init_time + compute_time + test_time);
+   return 0;
 }
