dde2cd6f70ab872d30ed4c65b574fd3b7640f3cb
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2025-07-16 14:14:59 -0300

    run atv21

---
+++ atv21/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition gpu-4-a100
+#SBATCH --gpus-per-node=1   # Número GPUs por nó
+#SBATCH --nodes 1
+#SBATCH --time 00:02:00
+#SBATCH --job-name vadd
+#SBATCH --output saida-%j.out
+
+cd $SLURM_SUBMIT_DIR
+
+ulimit -s unlimited
+
+mpirun -np 4 ./chapa
